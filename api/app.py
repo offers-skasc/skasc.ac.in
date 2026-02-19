6573c7465de26ce56cc552d9cf3c492d48c14662
offers-skasc <offers@skasc.ac.in>
2026-02-19 09:23:58 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -85,7 +85,7 @@ def google_form():
                         if (distance <= maxDistance) {{
                             document.getElementById('form-container').innerHTML = `
                                 <iframe 
-                                    src="https://forms.gle/yCEcPvpR66NB6QNK6"
+                                    src="https://forms.gle/5Mtt7BaiWmpaXEK6A"
                                     width="640" height="520" frameborder="0">
                                     Loading…
                                 </iframe>`;
